ee177aa4556daa4074bb95085f1a30fbfb2e9a82
anaya326 <58004376+anaya326@users.noreply.github.com>
2020-01-02 12:47:16 -0500

    I defined the colors that will be featured on the game board.

--- src/game.py
+++ src/game.py
@@ -1,9 +1,11 @@
-import time
-
-print("Welcome to the sample repository!")
-time.sleep(1)
-print("All the code for your game should go in the src folder")
-time.sleep(1)
-print("Now delete this file and make your own!")
-
 print("Welcome to our game of Tic Tac Toe")
+
+import pygame
+import sys
+
+Black = (0, 0, 0)
+White = (255, 255, 255)
+Light Coral = (240, 128, 128)
+Indian Red = (205, 92, 92)
+
+
