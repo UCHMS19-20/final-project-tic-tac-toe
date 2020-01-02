a8778fa9be2aa3bfbfbaf0482cd6361cbd2b4a1e
anaya326 <58004376+anaya326@users.noreply.github.com>
2020-01-02 12:23:49 -0500

    I made a welcome so players can know that they are playing tic tac toe.

--- src/game.py
+++ src/game.py
@@ -5,4 +5,5 @@ time.sleep(1)
 print("All the code for your game should go in the src folder")
 time.sleep(1)
 print("Now delete this file and make your own!")
-print("We are making Tic Tac Toe")
+
+print("Welcome to our game of Tic Tac Toe")
