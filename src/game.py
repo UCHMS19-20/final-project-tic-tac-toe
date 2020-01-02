25ca2e04aea0182b347f2e02b0e9eec81a319d17
anaya326 <58004376+anaya326@users.noreply.github.com>
2020-01-02 12:59:57 -0500

    I made the window display for the game
    git ad

--- src/game.py
+++ src/game.py
@@ -5,12 +5,28 @@ import sys
 
 Black = (0, 0, 0)
 White = (255, 255, 255)
-Light Coral = (240, 128, 128)
-Indian Red = (205, 92, 92)
+Pink = (240, 128, 128)
+DPink = (205, 92, 92)
 
 width = 3
 height = 3
 
 margin = 0.1
+
+grid = []
+for row in range(10):
+    # Added an empty array to hold each cell
+    # in this row
+    grid.append([])
+    for column in range(10):
+        grid[row].append(0)  # Append a cell
+
+pygame.init()
+# initialize pygame
+
+# Set the HEIGHT and WIDTH of the screen
+window_size = [255, 255]
+screen = pygame.display.set_mode(window_size)
+
 
 
