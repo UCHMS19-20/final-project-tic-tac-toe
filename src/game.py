a0d51d35d1e41c26b05628652565d2c4f211ac40
anaya326 <58004376+anaya326@users.noreply.github.com>
2020-01-10 12:33:23 -0500

    Update game.py

--- src/game.py
+++ src/game.py
@@ -1,1 +1,9 @@
+import pygame
 
+X1 = "x"
+# tells the turn; X1 means "x" goes first
+grid = [ [ None, None, None ], \
+         [ None, None, None ], \
+         [ None, None, None ] ]
+winner = none
+# declares supporting functions
