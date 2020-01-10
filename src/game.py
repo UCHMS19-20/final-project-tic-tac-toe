f0a4d6164d98ff284840ed3e8d56e7b8d6791bc8
anaya326 <58004376+anaya326@users.noreply.github.com>
2020-01-10 12:55:03 -0500

    Made a funtion to draw a grid

--- src/game.py
+++ src/game.py
@@ -5,5 +5,22 @@ X1 = "x"
 grid = [ [ None, None, None ], \
          [ None, None, None ], \
          [ None, None, None ] ]
-winner = none
+winner = None
 # declares supporting functions
+
+def __init__board(abc):
+    """init = initialize ; board is initialized and turned into a variable; 
+    "abc" is the pygame display variable"""
+    background = pygame.surface(abc.get_size())
+    background = background.convert()
+    background.fill = ((153,0,153))
+    # sets the background of the game a light purple color
+
+    pygame.draw.line(background, (0,0,0), (100,0), (100,300), 2)
+    pygame.draw.line(background, (0,0,0), (100,0), (200,300), 2)
+    # drawing vertical lines for the grid
+    pygame.draw.line(background, (0,0,0), (0,100), (300,100), 2)
+    pygame.draw.line(background, (0,0,0), (0,100), (300,200), 2)
+    # drawing horizontal lines for the grid
+    return background
+    # returns a drawn board
