29d00cdd30c7f3d85abff0164e5e0abc38612b56
anaya326 <58004376+anaya326@users.noreply.github.com>
2020-01-14 12:57:50 -0500

    I created a funtction that shows the position of the board and another function that draws the game pieces.

--- src/game.py
+++ src/game.py
@@ -29,5 +29,41 @@ def __init__board(abc):
         """the mouse gives coordinates to figure out
          the space on the board (row, column) the user clicked
          x_click = x coordinate & y_click = y coordinate  """
+        if (y_click < 100):
+                row = 0
+        elif (y_click < 200):
+                row = 1
+        else:
+                row = 2
+# shows the row clicked by the player
+        if (x_click < 100):
+                column = 0
+        elif (x_click < 200):
+                column = 1
+        else:
+                column = 2
+# shows the column clicked by the player
+
+def draw_move(board, b_row, b_col, piece):
+        """ board = surface of game,
+         b_row & b_col = row and column to draw the piece, 
+         piece = X or O"""
+
+        middle_X = ((b_col) * 100) + 50
+        middle_Y = ((b_row) * 100) + 50
+# shows center of the square board
+
+        if (piece == 'O'):
+                pygame.draw.circle (board, (0,0,0), (middle_X, middle_Y), 44, 2)
+        else:
+                pygame.draw.line (board, (0,0,0), (middle_X - 22, middle_Y - 22),
+                        (middle_X + 22, middle_Y + 22), 2)
+                pygame.draw.line (board, (0,0,0), (middle_X + 22, middle_Y - 22), 
+                        (middle_X - 22, middle_Y + 22), 2)
+# draws pieces X or O
+
+        grid[b_row][b_col] = piece
+# marks space as used
+
 
 
