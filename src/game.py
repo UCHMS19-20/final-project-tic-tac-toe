b21a7b38dde5db94781d1d2f7612341a94bc072f
scoulson21 <58479248+scoulson21@users.noreply.github.com>
2020-01-21 14:40:45 -0500

    Update game.py

--- src/game.py
+++ src/game.py
@@ -1,40 +1,5 @@
 import pygame
-<<<<<<< HEAD
-import sys
 
-width, height = 600, 600
-cell_count = 3
-cell_size = width/cell_count
-
-grid = []
-
-class Cell:
-
-    def __init__(self, x, y, sz):
-        self.x = x
-        self.y = y
-        self.sz = sz
-        self.filled = False
-        self.rectangle = pygame.Rect(self.x * sz, self.y * sz, sz, sz)
-
-    def show(self):
-        pygame.draw.rect(screen, pygame.Color("pink"), self.rectangle, 2)
-        if self.filled:
-            pygame.draw.rect(screen, pygame.Color("pink"), self.rectangle)
-
-pygame.init()
-
-screen = pygame.display.set_mode((width, height))
-
-for y in range(cell_count):
-    for x in range(cell_count):
-        grid.append(Cell(x, y, cell_size))
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-=======
 
 X1 = "x"
 # tells the turn; X1 means "x" goes first
@@ -100,13 +65,3 @@ def draw_move(board, b_row, b_col, piece):
 
         grid[b_row][b_col] = piece
 # marks space as used
->>>>>>> 29d00cdd30c7f3d85abff0164e5e0abc38612b56
-
-    # pygame.draw.rect(screen, pygame.Color("red"), (0,0,50,50))
-    for cell in grid:
-        if cell.rectangle.collidepoint(pygame.mouse.get_pos()):
-            cell.filled = True
-        cell.show()
-
-
-    pygame.display.flip() 
